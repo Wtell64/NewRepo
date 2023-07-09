5f35476d81de01dfd5acede25cfb401c10fb04c3
Wtell64 <75840262+Wtell64@users.noreply.github.com>
2023-07-09 10:57:11 +0300

    First commit

---
+++ IlkWebProjem/Models/Personel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IlkWebProjem.Models
+{
+
+
+    public class Personel
+    {
+        [Display(Name ="Personel Adi")]
+        [StringLength(50, ErrorMessage ="Bu alana en fazla 50 karakter yazilabilir")] //annotation
+        public string Ad { get; set; }
+
+        [DisplayName("Personel Soyadi")]
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+        public bool Aktif { get; set; }
+    }
+}
